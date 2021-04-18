274168b4dab302a24729899fceb06e96d849e0a2
RGV <phpngdeveloper@gmail.com>
2021-04-18 16:02:29 +0530

    Nodejs Backend APIS for Module

---
+++ config/config.js
@@ -0,0 +1,73 @@
+function getEnvValues(env) {
+    var result;
+
+    switch (env) {
+
+        case 'local':
+            result = {
+                "MONGO_URL": "mongodb://127.0.0.1:27017/crudapp",
+                "MONGO_DB":"crudapp",
+                'secret': 'ATHBKLOPCHKL',
+                'session_timeout':120, //in seconds,
+                "SENDGRID_API_KEY":"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",
+                "reset_url":"http://localhost:4200",
+                "from_email":'phpngdeveloper@gmail.com',
+            };
+            break;
+        case 'dev':
+            result = {
+                "MONGO_URL": "mongodb://127.0.0.1:27017/crudapp",
+                "MONGO_DB":"crudapp",
+                'secret': 'ATHBKLOPCHKL',
+                'session_timeout':120, //in seconds,
+                "SENDGRID_API_KEY":"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",
+                "reset_url":"http://localhost:4200",
+                "from_email":'phpngdeveloper@gmail.com',
+            };
+            break;
+        case 'QA':
+            result = {
+                "MONGO_URL": "mongodb://127.0.0.1:27017/crudapp",
+                "MONGO_DB":"crudapp",
+                'secret': 'supersecret',
+                'session_timeout':120, //in seconds,
+                "SENDGRID_API_KEY":"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",
+                "reset_url":"http://localhost:4200",
+                "from_email":'phpngdeveloper@gmail.com',
+            };
+            break;
+            
+        case 'cloud':
+            result = {
+                "MONGO_URL": "mongodb://127.0.0.1:27017/crudapp",
+                "MONGO_DB":"crudapp",
+                'secret': 'ATHBKLOPCHKL',
+                'session_timeout':120, //in seconds,
+                "SENDGRID_API_KEY":"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",
+                "reset_url":"http://localhost:4200",
+                "from_email":'phpngdeveloper@gmail.com',
+            };
+            break;
+            
+
+        default:
+            result = {
+                "MONGO_URL": "mongodb://127.0.0.1:27017/crudapp",
+                "MONGO_DB":"crudapp",
+                'secret': 'ATHBKLOPCHKL',
+                'session_timeout':120, //in seconds,
+                "SENDGRID_API_KEY":"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",
+                "reset_url":"http://localhost:4200",
+                "from_email":'phpngdeveloper@gmail.com',
+            };
+            break;
+
+    }
+
+
+    return result;
+
+
+}
+
+module.exports = { getEnvValues };
